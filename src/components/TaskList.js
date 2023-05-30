c38f6a3bd999f895d40055c3430ec4447acc6333
Jensen Lawless <jensenlawless@gmail.com>
2023-05-30 15:29:30 -0600

    complete

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,9 +1,15 @@
 import React from "react";
+import Task from "./Task";
 
-function TaskList() {
+function TaskList({ tasks, handleDelete }) {
+
   return (
     <div className="tasks">
-      {/* display a list of tasks using Task component */}
+      <ul>
+        {tasks.map((task) => (
+          <Task key={task.text} category={task.category} text={task.text} handleDelete={handleDelete} />))
+        }
+      </ul>
     </div>
   );
 }
